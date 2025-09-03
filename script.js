6f04cfec0673fdb815cb039883610c05f5a8dfe6
Ebaad Khan <ebaadcodemax@gmail.com>
2025-09-04 04:40:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+const populate = async (value, currency) => {
+
+    let mystr = "";
+
+    url = "https://api.currencyapi.com/v3/latest?apikey=cur_live_pshJnijI1tZ5DoDnH36JWAFMubLD0oMhL051ShJH&base_currency=" + currency
+    let response = await fetch(url)
+    let rjson = await response.json();
+
+
+    document.querySelector(".output").style.display="block";
+
+
+    document.querySelector("tbody").innerHTML = "";
+
+    for (let key of Object.keys(rjson["data"])) {
+
+        mystr += `<tr>
+    <td>${key}</td>
+    <td>${rjson["data"][key]["code"]}</td>
+    <td>${Math.round(rjson["data"][key]["value"] * value)}</td>
+</tr>`
+
+    }
+    const tablebody = document.querySelector("tbody");
+    tablebody.innerHTML = mystr;
+
+}
+
+
+const btn = document.querySelector('.btn')
+
+
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const value = parseInt(document.querySelector("input[name='quantity']").value);
+
+
+    const currency = document.querySelector("select[name='currency']").value;
+
+    populate(value, currency);
+});
+
+
+
+
